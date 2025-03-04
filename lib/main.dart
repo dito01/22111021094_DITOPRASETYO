3999d77186d7a89ab60a905a7a81b4602e22227f
ardyngantuk <2211102105@ittelkom-pwt.ac.id>
2025-03-04 10:43:43 +0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello World",
+      home: const MyHomePage(title: "Flutter Hello World Page"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(child: Text('Hello World', style: TextStyle(fontSize: 20))),
+    );
+  }
+}
